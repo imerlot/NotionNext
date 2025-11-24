37999fb1de1fdd19c11a9ef8a9a5365d6d10e792
imerlot <miro.gong@gmail.com>
2025-11-24 13:42:00 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -88,4 +88,3 @@ export default function Index(props) {
   console.log('Props:', JSON.stringify(props, null, 2)) // 查看是否为空
   return <div>Debug: {JSON.stringify(props, null, 2)}</div>
 }
-export default Index
