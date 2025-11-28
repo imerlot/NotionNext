0f8fcceb072a4397fda611949c0a3b691172d87d
imerlot <miro.gong@gmail.com>
2025-11-29 07:21:18 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,23 @@
 import BLOG from '@/blog.config'
 import { siteConfig } from '@/lib/config'
+import { getGlobalData } from '@/lib/db/getSiteData'
 import { DynamicLayout } from '@/themes/theme'
 
-const NoFound = () => {
-  // 安全的最小化配置
-  const NOTION_CONFIG = { THEME: BLOG.THEME }
-  const theme = siteConfig('THEME', BLOG.THEME, NOTION_CONFIG)
-  return <DynamicLayout theme={theme} layoutName='Layout404' />
+/**
+ * 404
+ * @param {*} props
+ * @returns
+ */
+const NoFound = props => {
+  const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)
+  return <DynamicLayout theme={theme} layoutName='Layout404' {...props} />
+}
+
+export async function getStaticProps(req) {
+  const { locale } = req
+
+  const props = (await getGlobalData({ from: '404', locale })) || {}
+  return { props }
 }
 
 export default NoFound
